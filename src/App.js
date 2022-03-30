cf5b2dddf115ee6c8aa03b2c0379d574950b4a29
Jake <mynonspamemailaddress@gmail.com>
2022-03-29 19:39:35 -0700

    [Front End][0/N]Landing page with pills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { useState } from 'react'
 import skull from './media/images/skull.png'
 import './App.css'
 import FormDataComponent from './components/FormDataComponent'
+import PillComponent from './components/PillComponent'
+import redEcstacyPill from './media/images/ecstacy_pill_red.jpg'
+import blueEcstacyPill from './media/images/ecstacy_pill_blue.jpg'
+
+import skullVideoFile from './media/videos/skullVideo.MP4'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -50,15 +55,40 @@ function App() {
       </div>
     </form>
 );
+  const redPill = (
+  <PillComponent 
+    imageSource={redEcstacyPill}
+    text="RED PILL"
+    >
+  </PillComponent>);
+  
+  const bluePill = (
+    <PillComponent 
+      imageSource={blueEcstacyPill}
+      text="BLUE PILL"
+      >
+    </PillComponent>)
+
+  const video = (
+    <video width="640" height="480" loop>
+      <source src={skullVideoFile} type="video/MP4"/>
+    </video>
+  );
 
   return (
     <div className="App">
       <header className="App-header">
-      <p>Unwrap & Steal</p>
-        <img src={skull} className="App-logo" alt="logo" />
-        {hasClicked === false ? formComponent : null}
-        {hasClicked === true ? buttonAfter : null}
-        <FormDataComponent />
+      <h1>THE AMIABIS</h1>
+        {video}
+        <h3>CHOOSE WISELY</h3>
+        <div className='PillSelection'>
+          <div className='RedPill'>
+            {redPill}
+          </div>
+          <div className='BluePill'>
+            {bluePill}
+          </div>
+        </div>
       </header>
     </div>
   )
---
+++ src/components/PillComponent.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+export default class PillComponent extends Component {
+    userData;
+    textColor;
+    constructor(props) {
+        super(props);
+        this.onSelectAffinity = this.onSelectAffinity.bind(this);
+        this.state = {
+            affinity: '',
+        }
+    }
+    // Form Events
+    onSelectAffinity(e) {
+        this.setState({ affinity: e.target.value })
+    }
+    // React Life Cycle
+    componentDidMount() {
+        if (this.props.text === 'RED PILL') {
+            this.textColor = 'red'
+        } else if (this.props.text === 'BLUE PILL') {
+            this.textColor = 'blue';
+        }
+
+        this.userData = JSON.parse(localStorage.getItem('affinity'));
+        if (localStorage.getItem('affinity')) {
+            this.setState({
+                affinity: ''
+            })
+        } else {
+            this.setState({
+                affinity: ''
+            })
+        }
+    }
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('affinity', JSON.stringify(nextState));
+    }
+
+    render() {
+        return (
+            <div className="pill">
+                    <img 
+                        height="100px" 
+                        width="100px" 
+                        src={this.props.imageSource} 
+                        alt="A choice." />
+            <p style={{ color: this.textColor }}>{this.props.text}</p>
+            </div>
+        )
+    }
+}
